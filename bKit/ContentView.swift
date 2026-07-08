a7b3727f4ccb4ba43c063fcb708aad68d8ef9f41
鲍小帅 <baoshuaipeng@163.com>
2026-07-08 14:51:21 +0800

    Initial Commit

---
+++ bKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bKit
+//
+//  Created by 鲍小帅 on 2026/7/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bKit/bKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  bKitApp.swift
+//  bKit
+//
+//  Created by 鲍小帅 on 2026/7/8.
+//
+
+import SwiftUI
+
+@main
+struct bKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
